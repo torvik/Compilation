4cf94e9aa3336348f2d8dc504a1196b3457febfd
torvik <pless.victor@gmail.com>
2015-01-28 22:51:09 -0200

    footer

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,9 @@
 class Project < ActiveRecord::Base
+
+  validates :nome, presence: true
+  validates  :horas_swf, presence: true
+  validates  :horas_sf, presence: true
+  validates  :faturado_sf, presence: true
+  validates  :faturado_swf, presence: true
+
 end
